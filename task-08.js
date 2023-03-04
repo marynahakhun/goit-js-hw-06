b9fc2f78aeda03be40a6e4381f4ca98639ec0437
Maryna Hakhun <114681951+marynahakhun@users.noreply.github.com>
2023-03-04 13:33:22 +0100

    Update task-08.js

--- task-08.js
+++ task-08.js
@@ -1,23 +1,23 @@
-const loginForm = document.querySelector('.login-form');
+
+const loginForm = document.querySelector('form.login-form');
 
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault(); 
-  
+
   const emailInput = loginForm.elements.email;
   const passwordInput = loginForm.elements.password;
-  
 
-  if (emailInput.value === '' || passwordInput.value === '') {
+  if (!emailInput.value || !passwordInput.value) {
     alert('Будь ласка, заповніть всі поля форми!');
-    return;
+    return; // Перервати подальше виконання скрипту
   }
-  
 
   const loginData = {
     email: emailInput.value,
-    password: passwordInput.value
+    password: passwordInput.value,
   };
-  
-  console.log(loginData); 
-  loginForm.reset(); 
+
+  console.log(loginData);
+
+  loginForm.reset();
 });
